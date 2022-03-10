9b7fc81c93902ecb95908dbbe00d5985dce34c6c
Johad Toledo <johadtoledo@gmail.com>
2022-03-10 21:55:37 +0100

    Bring the info from the rest api

--- devrockshop/src/components/Item.js
+++ devrockshop/src/components/Item.js
@@ -1,24 +1,25 @@
 import React, { useContext } from "react";
 import Context from "../context/Context";
 
-export default function Item() {
+export default function Item(props) {
+  const {img, nombre, precio, medidas} = props
   const { addProduct } = useContext(Context);
 
   return (
     <>
       <div className="home-item">
         <img
-          src="http://escueladevrock.com/store/0.png"
+          src={img}
           alt=""
           className="home-item-img"
         />
         <div className="home-item-info">
           <a href="producto.html">
-            <h1 className="home-item-titulo">Cerebro loco</h1>
+            <h1 className="home-item-titulo">{nombre}</h1>
           </a>
-          <p className="home-item-medidas">Medidas: 20x10</p>
+          <p className="home-item-medidas">Medidas: {medidas}</p>
           <div className="home-item-actions">
-            <h3 className="home-item-precio">AR$ 150</h3>
+            <h3 className="home-item-precio">AR$ {precio}</h3>
             <button
               className="home-item-comprar"
               onClick={() => {
--- devrockshop/src/containers/Home.js
+++ devrockshop/src/containers/Home.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import Item from '../components/Item'
+import Context from '../context/Context'
 
 export default function Home() {
+  useEffect(()=>{
+    getProducts()
+  })
+  const {getProducts, products} = useContext(Context)
   return (
     <>
     <div className="container">
       <div className="wraper">
         <div className="home">
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
+            {products.map((item)=>(
+              <>
+              <Item {...item}/>
+              </>
+
+            ))}
         </div>
       </div>
       </div>
